fb2104a6c5cc043290448683735b903158cbb2af
Andrew Backes <rbackes@ucla.edu>
2016-12-12 10:12:37 -0800

    Remove un-used

--- src/managing-acct/userdata.sh
+++ src/managing-acct/userdata.sh
@@ -2,16 +2,12 @@
 #wait for other services to come up
 echo "Sleeping for 20 seconds before executing scripts..."
 sleep 20
-#this is just a hack becase the image (AMI) is created with an overflow, i think
-hostname=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
-public_ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 
 cat <<EOT > /etc/default/armory
 ARMORY_S3_BUCKET=${s3_bucket}
 ARMORY_S3_FRONT50_PATH_PREFIX=${s3_front50_path_prefix}
 AWS_REGION=${aws_region}
-HOSTNAME=$hostname
-PUBLIC_IP=$public_ip
+HOSTNAME=${spinnaker_url}
 API_HOST=http://$hostname:8084
 DECK_HOST=0.0.0.0
 DECK_PORT=9000
