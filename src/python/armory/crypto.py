00dbc822a9884acf6083c617d62315f49968a3fd
Andrew Backes <rbackes@ucla.edu>
2017-01-24 12:47:29 -0800

    Moved integration tests to their own repo

--- src/python/armory/crypto.py
+++
@@ -1,8 +0,0 @@
-from Crypto.PublicKey import RSA
-
-def generate_keypair():
-    key = RSA.generate(2048)
-    private_pem = key.exportKey('PEM')
-    pubkey = key.publickey()
-    public_openssh = pubkey.exportKey(format='OpenSSH')
-    return private_pem.decode('utf-8'), public_openssh.decode('utf-8')
