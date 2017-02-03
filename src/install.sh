fc50fb684dbc7f642c064aa680786bd38688a8f2
Isaac Mosquera <imosquer@gmail.com>
2017-02-03 12:51:33 -0800

    fixes from review, removing uneeded vars, address language

--- src/install.sh
+++ src/install.sh
@@ -43,7 +43,9 @@ function describe_installer() {
     - IAM Policy Spinnaker ECR read access
 
 
-If you run into any issues during the installation, you can with us at http://go.armory.io/chat
+Need help, advice, or just want to say hello during the installation?
+Chat with our eng. team at http://go.Armory.io/chat.
+One customer called us “ridiculously responsive.”  Hopefully we can be the same for you!
 Press 'Enter' key to continue. Ctrl+c to quit.
 EOF
   read
@@ -126,11 +128,10 @@ function prompt_user() {
     get_var "Would you like to install Armory Spinnaker in a high availablity('ha') or development('stand-alone') configuration? [stand-alone|ha]:" TF_VAR_deploy_configuration
     get_var "Enter your AWS Profile [e.g. devprofile]: " AWS_PROFILE
     get_var "Enter S3 bucket to use for persisting Spinnaker's data[e.g. examplebucket]: " TF_VAR_s3_bucket
-    get_var "Enter S3 path prefix to use within the bucket [e.g. armory/config]: " TF_VAR_s3_front50_path_prefix
+    get_var "Enter S3 path prefix to use within the bucket [e.g. armory/config]: " TF_VAR_s3_prefix
     get_var "Enter an AWS Region. Spinnaker will be installed inside this region. [e.g. us-west-2]: " TF_VAR_aws_region
     get_var "Enter a VPC ID. Spinnaker will be installed inside this VPC. [e.g. vpc-7762cd13]: " TF_VAR_vpc_id
-    get_var "Enter a Subnet ID. Spinnaker will be installed inside this Subnet. [e.g. subnet-8f5d43d6]: " TF_VAR_armoryspinnaker_subnet_ids
-    get_var "Enter one or more AWS availablity zones. Spinnaker will be replicated within those zones. [e.g. us-west-2a,us-west2b]: " TF_VAR_availability_zones
+    get_var "Enter Subnet ID(s). Spinnaker will be installed inside this Subnet. Subnets cannot be in the same AZ [e.g. subnet-8f5d43d6,subnet-1234abcd]: " TF_VAR_armoryspinnaker_subnet_ids
     get_var "Enter a Key Pair name already set up with AWS/EC2. Spinnaker will be created using this key. [e.g. default-keypair]: " TF_VAR_key_name
 
     create_tmp_space
@@ -163,7 +164,7 @@ function create_spinnaker_stack() {
   source ${MP_FILE}
   run_terraform "remote config -backend=S3 \
     -backend-config=bucket=${TF_VAR_s3_bucket} \
-    -backend-config=key=${TF_VAR_s3_front50_path_prefix}/terraform/terraform.tfstate \
+    -backend-config=key=${TF_VAR_s3_prefix}/terraform/terraform.tfstate \
     -backend-config=region=${TF_VAR_aws_region} \
     -pull=true"
 
@@ -181,32 +182,6 @@ function wait_for_spinnaker() {
   echo "Log into your AWS console and find your external ELB URL"
   echo -e "If you run into any issues, you can with us at ${BLUE}http://go.armory.io/chat${NC}"
   exit 0
-  ## The code below will need changes in the TF files to output the correct ELB
-  local terraform_output=$(run_terraform "output" "./${TF_VAR_deploy_configuration}" | tr -d '\n\r')
-  local spinnaker_host=$(expr "${terraform_output}" : ".* spinnaker_url = \(.*\)}")
-  echo "Waiting for ${spinnaker_host} to become available."
-  spinnaker_url="http://${spinnaker_host}"
-
-  for i in {1..420}; do
-    echo -n "."
-    #we set +e so we don't error out when curl doesn't return 0
-    set +e
-    curl_result=$(curl -s --connect-timeout 2 ${spinnaker_url} &2>>/dev/null)
-    if [[ ${curl_result} != "" ]];then
-      echo ""
-      echo "Your Armory Spinnaker instance is up! Check it out: ${spinnaker_url}"
-      exit 0
-    fi
-    set -e
-    sleep 1
-  done
-  echo ""
-  echo "Couldn't connect to ${spinnaker_url}:9000. It is possible that partial"
-  echo "installation has occurred. You probably have AWS resources created "
-  echo "through this process that need to be cleaned up. You can find the "
-  echo ".tfstate file in the S3 bucket you specified: "
-  echo "    s3://${TF_VAR_s3_bucket}/${TF_VAR_s3_front50_path_prefix}"
-  exit 1
 }
 
 function main() {
