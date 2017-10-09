bd1b3177760fb83902ccc6158a7d0e9739875b82
jenkins <jenkins@armory.io>
2017-10-09 12:48:54 -0700

    Jenkins release update

--- src/install.sh
+++ src/install.sh
@@ -17,7 +17,7 @@ set -o pipefail
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 SOURCE_URL="http://get.armory.io/install/release"
-INSTALLER_PACKAGE_NAME=spinnaker-terraform-1.8.106.tar.gz
+INSTALLER_PACKAGE_NAME=spinnaker-terraform-1.9.95.tar.gz
 INSTALLER_PACKAGE_URL=${INSTALLER_PACKAGE_URL:-${SOURCE_URL}/${INSTALLER_PACKAGE_NAME}}
 TMP_PATH=${HOME}/tmp/armory
 TMP_PACKAGE_PATH=${TMP_PATH}/${INSTALLER_PACKAGE_NAME}
